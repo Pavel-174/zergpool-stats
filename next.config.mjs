0e2fdf03faca60ce4f2dbdf974217f84484afe97
Pavel-174 <alekseevps93@yandex.ru>
2024-12-18 11:14:55 +0500

    table

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
         return [
             {
               source: '/api/:path*',
-              destination: 'https://zergpool.com/:path*'
+              destination: 'https://zergpool.com/api/:path*'
             }
         ]
     }
--- src/app/statistic/page.js
+++ src/app/statistic/page.js
@@ -1,21 +1,69 @@
 "use client";
 import Header from "@/components/Header";
+import { useEffect, useState } from "react";
 
 export default function StatisticPage() {
+  const address = localStorage.getItem("wallet")
 
-  const fetchData = async () => {
-    const response = await fetch("api/api/wallet?address=151dtEMndfwcdW4PepzUhMZugcjfctaZXY");
-    console.log(response)
+  const [mainInfo, setMainInfo] = useState({})
+
+  const fetchData = async (address) => {
+    const response = await fetch(`api/walletEx?address=${address}`, {
+      method: 'GET',
+      headers: {
+        'Accept': 'application/json',
+      },
+    })
+    .then(response => response.json())
+    .then(response => setMainInfo(response))
   }
-    
-  fetchData();
+
+  useEffect(() => {
+    fetchData(address);
+  }, [address])
 
   return (
     <main className="flex flex-col gap-8 row-start-2 items-center">
       <Header />
-      <div>
-        
-      </div>
+      <h2 className="text-xl mt-6">Main info</h2>
+      <table className="w-10/12 border-separate border border-slate-300">
+        <tbody>
+          <tr className="flex items-start mx-auto p-4 border border-slate-300">
+            <td className="w-10/12">
+              Payment threshold
+            </td>
+            <td className="w-2/12 pl-2 flex flex-col">
+              {(mainInfo?.minpay + mainInfo?.currency).toString()}
+            </td>
+          </tr>
+          <tr className="flex items-start mx-auto p-4 border border-slate-300">
+            <td className="w-10/12">
+              Balance payment on Sunday late evening (CET timezone)  
+            </td>
+            <td className="w-2/12 pl-2 flex flex-col">
+              {(mainInfo?.minpay_sunday + mainInfo?.currency).toString()}
+            </td>
+          </tr>
+          <tr className="flex items-start mx-auto p-4 border border-slate-300">
+            <td className="w-10/12">
+              Estimated network tx fee for your payment (will be deducted from balance during payment)
+            </td>
+            <td className="w-2/12 pl-2 flex flex-col">
+              {('~'+ " " + mainInfo?.txfee).toString()}
+            </td>
+          </tr>
+          <tr className="flex items-start mx-auto p-4 border border-slate-300">
+            <td className="w-10/12">
+              Balance
+            </td>
+            <td className="w-2/12 pl-2 flex flex-col">
+              {(mainInfo?.balance + mainInfo?.currency).toString()}
+              <br/>
+              {((mainInfo?.balance / (mainInfo?.minpay / 100)).toFixed(2) + '%').toString()}
+            </td>
+          </tr>
+        </tbody>
+      </table>
     </main>
   );
 }
